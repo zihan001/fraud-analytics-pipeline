3ebcd2d34df76905aef454944316fae070bc0eaa
“Zihan <zihanhossain99@gmail.com>
2026-01-02 13:42:59 -0700

    fix: allow GitHub Actions OIDC from any ref

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -243,7 +243,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/${var.github_branch}"
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:*"
           }
         }
       }
